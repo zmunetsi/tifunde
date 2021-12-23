9d5665fbb3adfd233e6f89101fd5244676f93bae
Munetsi Zunguzira <dev@jetweb.onmicrosoft.com>
2021-12-23 14:07:55 +0200

    update database.php for jawdb

--- config/database.php
+++ config/database.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Str;
-$url = getenv('JAWSDB_URL');
-$dbparts = parse_url($url);
 
 return [
 
@@ -47,18 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            // 'url' => env('DATABASE_URL'),
-            // 'host' => env('DB_HOST', '127.0.0.1'),
-            // 'port' => env('DB_PORT', '3306'),
-            // 'database' => env('DB_DATABASE', 'forge'),
-            // 'username' => env('DB_USERNAME', 'forge'),
-            // 'password' => env('DB_PASSWORD', ''),
-            'url' => getenv('JAWSDB_URL'),
-            'host' => $dbparts['host'],
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => ltrim($dbparts['path'],'/'),
-            'username' => $dbparts['user'],
-            'password' => $dbparts['pass'],
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
